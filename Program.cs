e68973b7eeefeba9ab81dcce1bb8431adc21dee8
huseyinemrekantas <huseyinemrekantas@gmail.com>
2023-03-03 03:13:13 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿// See https://aka.ms/new-console-template for more information
+using System;
+namespace if_else_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if (time >= 6 && time < 11)
+            {
+                Console.WriteLine("Günaydın!");
+            }else if(time <= 18)
+            {
+                
+                Console.WriteLine("İyi günler!");
+            }else
+            {
+                
+                Console.WriteLine("İyi Geceler!");
+            }
+            string sonuc = time <= 18 ? "İyi Günler!" : "İyi Geceler!";
+            sonuc = time >= 6 && time <= 10 ? "Günaydın!" : time <= 18 ? "İyi Günler!" : "İyi Geceler!";
+            Console.WriteLine(sonuc);
+        } 
+
+    }
+}
+
